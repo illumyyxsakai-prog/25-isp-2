bca14198fd324574063b92e282636f65cf258181
illumy <illumy@511-03>
2026-09-12 14:44:22 +0200

    Добавьте файлы проекта.

---
+++ asd/Program.cs
@@ -0,0 +1,34 @@
+﻿////Console.WriteLine("dsadsdasd");
+////    string name = Console.ReadLine();
+////Console.WriteLine("Меня зовут" + name);
+////Console.WriteLine("Меня зовут {$}",name);
+////Console.WriteLine($"Меня зовут {name}");
+////Console.Write("Введите x:");
+////int x = int.Parse(Console.ReadLine());
+////Console.Write("введите y:");
+////int y = Convert.ToInt32(Console.ReadLine());
+////int s = x + y;
+////Console.WriteLine($"[x]+[y]=[s]");
+///
+//float x = 3.7F;
+//double v = Math.Pow(x, 7);
+//Console.Write("Введите угол в градусах:");
+//double angle = double.Parse(Console.ReadLine());
+//double y = Math.Sin(angle * Math.PI / 180);
+//Console.WriteLine($"y={y:f2}");
+
+//Console.Write("Введите степень числа:");
+//double x = double.Parse(Console.ReadLine());
+//double y = Math.Exp(7*x);
+//Console.WriteLine($"y={y:f2}");
+
+//лабораторная работа #0125021210
+
+
+
+
+
+
+
+
+
---
+++ lab1_1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Console.Write("Введите x:");
+//double x = double.Parse(Console.ReadLine());
+//double K = (Math.Sqrt(Math.Pow(3 + x, 6) - Math.Log
+//    (x))) / (Math.Exp(0) + Math.Asin(6 * x * x));
+//Console.WriteLine($"K={K:F2}");
+
+//высокий 6
+
+Console.Write("введите x:");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите b:");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Введите a:");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите y:");
+double y = double.Parse(Console.ReadLine());
+
+double K = (Math.Sqrt(x + b - a) + Math.Log(y))/(Math.Atan(b + a));
+Console.WriteLine($"K={K:F2}");
+
+
+
+
+
+
+
+
+
